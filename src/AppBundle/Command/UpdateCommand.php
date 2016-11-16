9b0141e66e83b1b1245cc94ac9aa7017d42fcfd8
ilia <ilya4041@yandex.ru>
2016-11-17 02:31:41 +0300

    update command

--- src/AppBundle/Command/UpdateCommand.php
+++ src/AppBundle/Command/UpdateCommand.php
@@ -6,6 +6,7 @@ use AppBundle\Utils\GoogleUtils;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Doctrine\ORM\EntityManager;
 
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -14,16 +15,20 @@ class UpdateCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
-        $this->setName('app:update') ;
+        $this->setName('app:update')
+            ->addArgument('client_id', InputArgument::OPTIONAL, 'Optional client id');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $em = $this->getContainer()->get('doctrine.orm.entity_manager');
-        $clients = $em->getRepository('AppBundle:Client')->findAll();
+        $client_id = $input->getArgument('client_id');
+        if($client_id != null)
+            $clients = [$em->getRepository('AppBundle:Client')->find($client_id)];
+        else
+            $clients = $em->getRepository('AppBundle:Client')->findAll();
         $redirect_uri = $this->getContainer()->get('router')->generate('google_redirect', [], UrlGeneratorInterface::ABSOLUTE_URL);
         $gClient = GoogleUtils::getGoogleClient($redirect_uri);
-
         foreach ($clients as $client){
             $gClient->setAccessToken($client->getToken());
             try {
